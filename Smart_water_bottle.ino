37f89aa7d3284d6a836c177b0184e34e80fce184
Shao-Qian Mah <shaoqianmah@gmail.com>
2016-11-02 11:11:10 +0800

    Updated code to ALPHA 1.0
    Functional total water counter, converts any ml above or equal to 1000 to L.

--- Smart_water_bottle.ino
+++ Smart_water_bottle.ino
@@ -1,8 +1,7 @@
 volatile int flow_frequency; // Measures flow sensor pulses
-unsigned int ml_sec; // Calculated millilitres/second
-unsigned int total_ml;
-
-unsigned char flowsensor = 2; // Sensor Input [UPDATE WITH PIN]
+float ml_sec; // Calculated ml/sec
+float total_water; // Total water consumed, in ml
+unsigned char flowsensor = 2; // Sensor Input [PIN NUMBER UPDATE AS REQUIRED]
 unsigned long currentTime;
 unsigned long cloopTime;
 void flow () // Interrupt function
@@ -18,23 +17,25 @@ void setup()
    sei(); // Enable interrupts
    currentTime = millis();
    cloopTime = currentTime;
-   total_ml= 0;
+   total_water = 1500;
 }
-
-
-void loop () //Add repeated functions here
+void loop ()
 {
    currentTime = millis();
-   // Every second, calculate and print ml/sec
+   // Every second, calculate and print litres/hour
    if(currentTime >= (cloopTime + 1000))
    {
       cloopTime = currentTime; // Updates cloopTime
-      // Pulse frequency (Hz) = 8.1Q, Q is flow rate in L/min.
-      ml_sec = (flow_frequency / ( 60 * 8.1 * 1000 ) ); // (Pulse frequency) / 8.1Q * 60 seconds * 1000 ml = flowrate in ml/second
-      total_ml = total_ml + ml_sec;
-      
+      // Pulse frequency (Hz) = 8.1, Q is flow rate in L/min.
+      ml_sec = (flow_frequency * 2.0576); // (Pulse frequency x 60) / 8.1 Q = flowrate in L/hour, therefore (Pulse frequency x 1000 / 8.1Q x 60) is flowrate in ml/sec
+      total_water = total_water + ml_sec;
       flow_frequency = 0; // Reset Counter
-      Serial.print(total_ml, DEC);
-      Serial.println(" total ml");
+      if (total_water >= 1000) {
+        Serial.print( total_water / 1000 ); // Total water in L
+        Serial.println(" L consumed");
+      } else {
+        Serial.print( total_water ); // Total water in ml
+        Serial.println(" ml consumed");
+      }
    }
 }
